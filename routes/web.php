f0d95e0c25a73499ce774fa06292ce945584c173
Mohammad Sulaiman <smensulaiman007@gmail.com>
2022-12-02 18:09:15 +0900

    new controller added

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
-Route::get('/', 'PagesController@Index');
+Route::get('/', 'App\Http\Controllers\PagesController@Index');
 
 Route::get('/hello', function () {
     return '<h1>Hello World</h1>';
